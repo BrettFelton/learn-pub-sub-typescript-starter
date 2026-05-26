86479ade5343c7237be207afe2279dcd9b89c063
Brett Felton <bmfelton@live.com>
2026-05-26 14:09:34 +0200

    Added Exchange que and channel

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,26 @@
+import amqp from "amqplib"
+import { publishJSON } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  const ch = await conn.createConfirmChannel();
+  console.log("Peril game server connected to RabbitMQ!");
+  publishJSON(ch, ExchangePerilDirect, PauseKey, { isPaused: true });
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
